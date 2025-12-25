8f69a0c24d1b2ed7c42a63cd05aa4e50dc0d714d
Danyaal07 <danyaalhashmi@icloud.com>
2025-12-25 04:29:20 +0000

    Restore sliding window max sum solution

---
+++ day04_queues_sliding_window/sliding_window_max_sum.py
@@ -0,0 +1,12 @@
+nums = [2, 1, 5, 1, 3, 2]
+k = 3
+
+windowSum = sum(nums[:k])
+maxSum = windowSum
+
+for i in range(len(nums) - k):
+    windowSum = windowSum - nums[i] + nums[k + i]
+    if windowSum > maxSum:
+        maxSum = windowSum
+
+print(maxSum)
